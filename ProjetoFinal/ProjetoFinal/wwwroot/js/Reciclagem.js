ed88f256a8a38e14dba0865fb0041d65ea696095
“Yasmim <yasmin.dorta@aluno.senai.br>
2024-10-25 15:35:58 -0300

    quarta

---
+++ ProjetoFinal/ProjetoFinal/wwwroot/js/Reciclagem.js
@@ -0,0 +1,20 @@
+﻿// Espera o documento estar completamente carregado
+document.addEventListener("DOMContentLoaded", function () {
+    // Inicializa o carrossel
+    var carousel = new bootstrap.Carousel(document.querySelector('#carouselExampleIndicators'), {
+        interval: 3000, // Intervalo entre os slides em milissegundos (ex: 3000ms = 3s)
+        pause: 'hover' // Pausa o carrossel quando o mouse está sobre ele
+    });
+
+    // Adiciona eventos para os controles
+    var prevButton = document.querySelector('.carousel-control-prev');
+    var nextButton = document.querySelector('.carousel-control-next');
+
+    prevButton.addEventListener('click', function () {
+        carousel.prev();
+    });
+
+    nextButton.addEventListener('click', function () {
+        carousel.next();
+    });
+});
