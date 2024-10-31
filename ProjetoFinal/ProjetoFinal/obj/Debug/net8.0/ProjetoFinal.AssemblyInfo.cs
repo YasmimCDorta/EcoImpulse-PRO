b3bcbe5e93a6ea413763cfbed867c6953ce6e69d
“Yasmim <yasmin.dorta@aluno.senai.br>
2024-10-31 12:48:59 -0300

    sexta

--- ProjetoFinal/ProjetoFinal/obj/Debug/net8.0/ProjetoFinal.AssemblyInfo.cs
+++ ProjetoFinal/ProjetoFinal/obj/Debug/net8.0/ProjetoFinal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoFinal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed88f256a8a38e14dba0865fb0041d65ea696095")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d1d1ac2c365c6b289573ccda73a45e63c2c8810")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoFinal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoFinal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
